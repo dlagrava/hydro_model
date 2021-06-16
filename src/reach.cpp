d4030521ecbebdf1f7eb3e5a02916b75c5ec87e7
dlagrava <dlagrava@gmail.com>
2021-06-16 14:48:19 +1200

    Adding base class for reaches

---
+++ src/reach.cpp
@@ -0,0 +1,15 @@
+#include <hydro_model/reach.hpp>
+
+/**
+ * Default constructor
+ * Used to create an empty reach and fill it later
+ */
+Reach::Reach() : 
+    latitude{ 0.0 },
+    longitude{ 0.0 },
+    properties{ ReachProperties() }
+{}
+
+
+
+
